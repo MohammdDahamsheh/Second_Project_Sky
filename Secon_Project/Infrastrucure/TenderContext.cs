51574ff4dd0b85ed1634f99c37615a4267b99e9d
mohammd jehad <mhmddahamsheh@gmail.com>
2025-12-08 17:59:50 +0300

    Build the DDD strucure

---
+++ Secon_Project/Infrastrucure/TenderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastrucure
+{
+    internal class TenderContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-NRLBL9O\\SQLEXPRESS01;database=ProjectTwoSkyDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
